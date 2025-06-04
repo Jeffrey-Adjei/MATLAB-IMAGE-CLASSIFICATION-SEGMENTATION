422b0150f0d78d9e67921ac0dbc693bf92fc7c45
Jeffrey-Adjei <118208940+Jeffrey-Adjei@users.noreply.github.com>
2025-06-04 03:18:37 +0100

    successfully trained the CNN however accuracy needs to be improved (60% accuracy)

--- classification.m
+++ classification.m
@@ -7,4 +7,47 @@ inputSize = [256 256 3];
 augimdsTrain = augmentedImageDatastore(inputSize, imdsTrain);
 augimdsValidation = augmentedImageDatastore(inputSize, imdsValidation);
 
+disp(imdsTrain);
+disp(imdsValidation);
 
+countEachLabel(imdsTrain)
+countEachLabel(imdsValidation)
+
+layers = [
+    imageInputLayer(inputSize)
+
+    convolution2dLayer(3, 8, 'padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+
+
+    convolution2dLayer(3, 16, 'padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+
+
+    convolution2dLayer(3, 32, 'padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, "Stride", 2)
+
+
+    fullyConnectedLayer(17)
+    softmaxLayer
+    classificationLayer
+    ];
+
+
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate', 0.001, ...
+    'MaxEpochs', 10, ...
+    'MiniBatchSize', 32, ...
+    'Shuffle', 'every-epoch', ...
+    'ValidationData', augimdsValidation, ...
+    'ValidationFrequency', 30, ...
+    'Verbose', false, ...
+    'Plots', 'training-progress');
+
+net = trainNetwork(augimdsTrain, layers, options);
